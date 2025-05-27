ef90699b43afb195a0a1a25e379cd7c74e397ccd
Stepan <afanasevstepan67@gmail.com>
2025-05-27 20:53:11 +0400

    Initial commit

---
+++ software_program/src/main/java/com/software/software_program/SoftwareProgramApplication.java
@@ -0,0 +1,13 @@
+package com.software.software_program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareProgramApplication.class, args);
+	}
+
+}
---
+++ software_program/src/test/java/com/software/software_program/SoftwareProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.software_program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
